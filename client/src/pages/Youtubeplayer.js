5cbcf7c94c98b8c433c6329c71ae21c46ea4284f
samkim <milk3531@hotmail.com>
2023-07-09 22:19:30 -0700

    Added button to videoPlayer

--- client/src/pages/Youtubeplayer.js
+++ client/src/pages/Youtubeplayer.js
@@ -1,14 +1,28 @@
-export const Youtubeplayer = ({videoIds}) => {
+export const Youtubeplayer = ({ videoIds }) => {
+
+    const buttonHandler = (videoSrc) => (event) => {
+        event.preventDefault();
+        console.log("Current video source:", videoSrc);
+      };
+      
+
     return (
         <div>
-            {videoIds.map((videoId,index) =>
-                <iframe key= {index} width="560" height="315" src={`https://www.youtube.com/embed/${videoId}`}
-                    title="YouTube video player" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen></iframe>
-            )}
+          {videoIds.map((videoId) => (
+            <div key={videoId} className="video-container">
+                <button className="save-button" onClick={buttonHandler(videoId)}>Save video</button>
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ))}
         </div>
-    )
+      );
 }
 
 
