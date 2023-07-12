556453e453e0281eaed233a0f86092c5324f30d0
samkim <milk3531@hotmail.com>
2023-07-12 00:11:29 -0700

    Change UserSchema
    savedVideos has type string, instead of objectId

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {type: String, required : true, unique : true},
     password: {type: String, required : true},
-    savedVideos: [{type: mongoose.Schema.Types.ObjectId, ref: "videos"}]
+    savedVideos: [{type: String, ref: "videos"}]
 });
 
 export const UserModel = mongoose.model("users", userSchema);
