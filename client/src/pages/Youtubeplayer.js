5d894499da74d12aa6b732641199bc5cfabe06fe
samkim <milk3531@hotmail.com>
2023-07-13 23:45:51 -0700

    Button disabled on click for saving

--- client/src/pages/Youtubeplayer.js
+++ client/src/pages/Youtubeplayer.js
@@ -4,12 +4,12 @@ import { useState } from "react";
 
 export const Youtubeplayer = ({ videoIds }) => {
   const myid = userID();
-  const [click, setClick] = useState(false);
 
   const buttonHandler = (videoSrc) => (event) => {
     event.preventDefault();
+    event.currentTarget.disabled = true;
     axios.put("http://localhost:3001/videoInfo", { src: videoSrc, id: myid }); // Send save video request
-    setClick(true);
+    
     console.log("Current video source:", videoSrc);
   };
 
@@ -18,7 +18,7 @@ export const Youtubeplayer = ({ videoIds }) => {
     <div>
       {videoIds.map((videoId) => (
         <div key={videoId} className="video-container">
-          <button className="save-button" disabled={click} onClick={buttonHandler(`https://www.youtube.com/embed/${videoId}`)}>Save video</button>
+          <button className="save-button" onClick={buttonHandler(`https://www.youtube.com/embed/${videoId}`)}>Save video</button>
           <iframe
             width="560"
             height="315"
