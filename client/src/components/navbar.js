a9f665f72d8e26a285d017c3468336ba4634c0ab
samkim <milk3531@hotmail.com>
2023-09-01 18:12:18 -0700

    Adding login/logout to prevent logging in again

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const [login, setLogin] = useState();
   const navigate = useNavigate();
 
   const logout = () => {
-    window.localStorage.clear();
+    window.localStorage.removeItem("userID");
     navigate("/auth");
   };
+
+  useEffect(() => {
+    const isLoggedin = window.localStorage.getItem("userID");
+    if (isLoggedin) {
+      console.log(isLoggedin);
+      setLogin(<Link className={"link-styles"} to="/auth" onClick={logout}>
+        Logout
+      </Link>)
+    } else {
+      setLogin(<Link className={"link-styles"} to="/auth">
+        Login
+      </Link>)
+    }
+  },[window.localStorage.getItem("userID")])
+
   return (
     <div className="navbar">
       <Link className={"link-styles"} to="/">
         Home
       </Link>
-      <Link className={"link-styles"} to="/auth">
-        Login
-      </Link>
+      {login}
       <Link className={"link-styles"} to="/saved">
         Saved Videos
       </Link>
