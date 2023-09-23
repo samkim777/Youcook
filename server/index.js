ace5dc19335f862e47fe70a53856fd0f7c632fd9
samkim <milk3531@hotmail.com>
2023-09-23 16:22:54 -0700

    micro-cors added

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ import dotenv from "dotenv";
 import { userRouter } from "./routes/user.js";
 import videoRouter from "./routes/search.js";
 import { savedRouter } from "./routes/savedVideos.js";
+import {micro} from 'micro';
+import {mcors} from 'micro-cors';
 
 dotenv.config();
 const app = express();
+const send = micro();
 
 // Middleware
 app.use(express.json());
@@ -18,6 +21,20 @@ app.use("/saved", savedRouter);
 
 // Connect to DB
 mongoose.connect(process.env.MONGODB_URI);
+
+const handler = (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return send(res, 200, 'ok!');
+  }
+
+  if (req.method !== 'POST') {
+    throw createError(404, 'Not Found');
+  }
+
+  // handle incoming request as usual
+}
+
+module.exports = cors(handler)
 
 app.listen(3001, () => {
   console.log("Server started on port 3001");
