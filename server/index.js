54e7153a7820f447974d854463ba6a71660ce82e
samkim <milk3531@hotmail.com>
2023-09-23 16:49:07 -0700

    get rid of micro cors

--- server/index.js
+++ server/index.js
@@ -5,12 +5,9 @@ import dotenv from "dotenv";
 import { userRouter } from "./routes/user.js";
 import videoRouter from "./routes/search.js";
 import { savedRouter } from "./routes/savedVideos.js";
-import {micro} from 'micro';
-import {mcors} from 'micro-cors';
 
 dotenv.config();
 const app = express();
-const send = micro();
 
 // Middleware
 app.use(express.json());
@@ -22,19 +19,7 @@ app.use("/saved", savedRouter);
 // Connect to DB
 mongoose.connect(process.env.MONGODB_URI);
 
-const handler = (req, res) => {
-  if (req.method === 'OPTIONS') {
-    return send(res, 200, 'ok!');
-  }
 
-  if (req.method !== 'POST') {
-    throw createError(404, 'Not Found');
-  }
-
-  // handle incoming request as usual
-}
-
-module.exports = cors(handler)
 
 app.listen(3001, () => {
   console.log("Server started on port 3001");
